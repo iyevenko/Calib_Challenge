1ed559b9329ae9ecbe81f27ca5433c9ff4607e64
Ivan Yevenko <iyevenko@gmail.com>
2021-06-24 01:11:18 -0400

    Display results from SfMLearner

--- explore.py
+++ explore.py
@@ -36,7 +36,7 @@ def plot_and_play():
 
 
 if __name__ == '__main__':
-    idx = 2
+    idx = 3
     label_fname = f'{idx}.txt'
     video_fname = f'{idx}.hevc'
     label_path = DATA_PATH + label_fname
@@ -45,6 +45,7 @@ if __name__ == '__main__':
     with open(label_path, 'r') as f:
         data = f.readlines()
         data = [[float(x) for x in line.split(' ')] for line in data]
+        pred_data = np.load(DATA_PATH + '3.npy')
         # plt.title(label_fname)
         # plt.plot(data)
         # plt.show()
@@ -57,6 +58,7 @@ if __name__ == '__main__':
         ret = True
         i = 0
         R = 910
+        scale = 10000
         while cap.isOpened() and ret:
             ret, frame = cap.read()
             if ret:
@@ -64,8 +66,11 @@ if __name__ == '__main__':
                 pitch, yaw = np.nan_to_num(data[i])
                 x = int(W/2 + R*np.sin(yaw))
                 y = int(H/2 + R*np.sin(pitch))
+                pred_x = int(W/2 + scale*pred_data[i,0])
+                pred_y = int(H/2 + scale*pred_data[i,1])
                 cv2.drawMarker(frame, (W//2, H//2), (255,0,0), cv2.MARKER_CROSS, 10)
                 cv2.drawMarker(frame, (x, y), (0,255,0), cv2.MARKER_CROSS, 10)
+                cv2.drawMarker(frame, (pred_x, pred_y), (0,0,255), cv2.MARKER_CROSS, 10)
                 cv2.imshow(video_fname, frame)
                 if cv2.waitKey(1000//30) & 0xFF == ord('q'):
                     break
